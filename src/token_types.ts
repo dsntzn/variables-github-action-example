396deb38b1e53f6a3eed84f5606f218b9beb1051
dsntzn <brooklynpd@gmail.com>
2025-02-13 09:24:45 +0100

    update allowed types

--- src/token_types.ts
+++ src/token_types.ts
@@ -14,8 +14,10 @@ export interface Token {
    *
    * We allow `string` and `boolean` types in addition to the draft W3C spec's `color` and `number` types
    * to align with the resolved types for Figma variables.
+   *
+   * We also allow 'dimension', 'fontFamily' and 'fontWeight in addition to the core repository
    */
-  $type: 'color' | 'number' | 'string' | 'boolean'
+  $type: 'color' | 'number' | 'string' | 'boolean' | 'dimension' | 'fontFamily' | 'fontWeight'
   $value: string | number | boolean
   $description?: string
   $extensions?: {
